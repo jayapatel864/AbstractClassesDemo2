bf4a21cc16070c5c102f12fa5aee79528c7fa350
unknown <jayapatel864@gmail.com>
2020-06-29 14:15:39 -0500

    added files

---
+++ repos/CarLotSimulator/CarLotSimulator/CarLotSimulator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+
+
+namespace CarLotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jayasCar = new Car();
+            jayasCar.Make = "Honda";
+            jayasCar.Model = "CRV";
+            jayasCar.Year = 2016;
+            jayasCar.EngineNoise = "vroom";
+            jayasCar.HonkNoise = "veep";
+            jayasCar.IsDriveable = true;
+
+            var dadsCar = new Car();
+            dadsCar.Make = "Audi";
+            dadsCar.Model = "A 8";
+            dadsCar.Year = 2014;
+            dadsCar.EngineNoise = "broom";
+            dadsCar.HonkNoise = "beep";
+            dadsCar.IsDriveable = true;
+
+               
+
+        }
+    }
+}
